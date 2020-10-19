9d1ff62b80fa539e363b179ff1a343765c54a5ea
Igor Andrade Silva <igor.andrade1013@gmail.com>
2020-10-19 08:06:44 -0300

    Creating interfaces

---
+++ AppMvcCompleta/src/DevIO.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,30 @@
+﻿using AppMvcBasica.Models;
+using DevIO.Business.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repository
+{
+    class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking().Include(navigationPropertyPath: f => f.Fornecedor)
+                .FirstOrDefaultAsync(predicate: p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking().Include(navigationPropertyPath: f => f.Fornecedor)
+                .OrderBy(p => p.Nome).ToListAsync();
+        }
+    }
+}
